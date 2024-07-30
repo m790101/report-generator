4569ae9863c53bef7994d946a2f816bd06e03720
m790101 <m790101@hotmail.com>
2024-07-31 02:06:54 +0800

    finish dowload feature

--- src/app/compoment/table/table.component.ts
+++ src/app/compoment/table/table.component.ts
@@ -9,9 +9,13 @@ import {
   output,
 } from '@angular/core';
 import moment from 'moment';
-import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import { IconDownloadComponent } from '../icon-download/icon-download.component';
+import autoTable from 'jspdf-autotable'
+import '../../../assets/font/NotoSansTC-Regular-normal'
+import html2canvas from 'html2canvas';
+import { borderBottomWidth } from 'html2canvas/dist/types/css/property-descriptors/border-width';
+
 
 @Component({
   selector: 'app-table',
@@ -178,51 +182,30 @@ export class TableComponent {
 
   generatePdf(data: any) {
 
-    html2canvas(data, { allowTaint: true }).then((canvas) => {
-      let HTML_Width = canvas.width;
-      let HTML_Height = canvas.height;
-      let top_left_margin = 100;
-      let PDF_Width = HTML_Width + top_left_margin * 2;
-      let PDF_Height = PDF_Width * 1.5 + top_left_margin * 2;
-      let canvas_image_width = HTML_Width;
-      let canvas_image_height = HTML_Height;
-      let totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
-      canvas.getContext('2d');
-      let imgData = canvas.toDataURL('image/jpeg', 1.0);
-      let pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
-      pdf.addImage(
-        imgData,
-        'JPG',
-        top_left_margin,
-        top_left_margin,
-        canvas_image_width,
-        canvas_image_height
-      );
-      for (let i = 1; i <= totalPDFPages; i++) {
-        pdf.addPage([PDF_Width, PDF_Height], 'p');
-        pdf.addImage(
-          imgData,
-          'JPG',
-          top_left_margin,
-          -(PDF_Height * i) + top_left_margin * 4,
-          canvas_image_width,
-          canvas_image_height
-        );
-      }
-      pdf.save('HTML-Document.pdf');
-    });
+    var doc = new jsPDF();
+    const header = this.data()[0].name + ':'
+    doc.setFont("NotoSansTC-Regular");
+    doc.setFontSize(12)
+    doc.text(header, 15, 10)
+
+    doc.setTextColor('#58636d')
+    autoTable(doc,{html:'#pdfTable',
+      useCss:true,
+      styles: {
+      font: 'NotoSansTC-Regular',
+      fontStyle: 'normal',
+      lineWidth: {
+        bottom: 1,
+        top: 0,
+        left: 0,
+        right: 0,
+      },
+    },
+
+  })
+    doc.save('table.pdf');
   }
-  // exportAsPDF(data:any)
-  // {
-  //   // const pdfTable = this.pdfTable.nativeElement;
-  //     html2canvas(data).then(canvas => {
-  //     const contentDataURL = canvas.toDataURL('image/png',1.0)  // 'image/jpeg' for lower quality output.
-  //     let pdf = new jsPDF('l', 'cm', 'a3'); //Generates PDF in landscape mode
-  //     // let pdf = new jspdf('p', 'cm', 'a4'); Generates PDF in portrait mode
-  //     pdf.addImage(contentDataURL, 'PNG', 0, 0, 29.7, 21.0);
-  //     pdf.save('Filename.pdf');
-  //   });
-  // }
+
 
   hasValue(key: any): boolean {
     return this.data().some(
@@ -400,3 +383,10 @@ interface DataItem {
   RA?: string;
   STSRPR?: string;
 }
+
+
+// var callAddFont = function () {
+//   this.addFileToVFS('NotoSansTC-Regular-normal.ttf', font);
+//   this.addFont('NotoSansTC-Regular-normal.ttf', 'NotoSansTC-Regular', 'normal');
+//   };
+//   jsPDF.API.events.push(['addFonts', callAddFont])
